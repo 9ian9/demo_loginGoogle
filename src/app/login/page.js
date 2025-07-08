deed38ca847b793a9cb2c4baa4ab504846ef6959
khanhha2711 <khanhha27112003@gmail.com>
2025-07-08 09:04:15 +0700

    update font

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,9 +1,10 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter,useSearchParams } from 'next/navigation';
 import api from '@/lib/axiosInstance';
 import LoginWithEmail from '@/components/login/LoginWithEmail.js';
+import LoginWithGoogle from '@/components/login/LoginWithGoogle';
 
 function Login() {
   const router = useRouter();
@@ -55,6 +56,8 @@ function Login() {
 
     checkAccessToken();
   }, []);
+  const searchParams=useSearchParams();
+  const error =searchParams.get("error");
 
     return(
         <div className="bg-white flex justify-center items-center h-screen">
@@ -69,12 +72,8 @@ function Login() {
                     <div className="flex flex-col gap-4">
                         <LoginWithEmail onLogin={toggleClickSignIn} />
                         <div className="divider h-5 m-0 text-xs">or</div>
-                        <button className="btn bg-white text-black border-[#e5e5e5] rounded-[8] h-12">
-                            <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
-                            Login with Google
-                        </button>
+                        <LoginWithGoogle/>
                     </div>
-
                 </div>
             </div>
         </div>
