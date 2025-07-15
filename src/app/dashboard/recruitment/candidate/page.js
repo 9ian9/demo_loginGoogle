30b64fc59a246d08ba4535ff557c0f8cb17b1170
khanhha2711 <khanhha27112003@gmail.com>
2025-07-15 10:29:35 +0700

    update candidate

--- src/app/dashboard/recruitment/candidate/page.js
+++ src/app/dashboard/recruitment/candidate/page.js
@@ -1,39 +1,39 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import HeaderContent, { BreakCrumbs } from "@/components/HeaderContent";
 import ItemCount from "@/components/recruitment/ItemCount";
 import TableAllPosition from "@/components/recruitment/TableAllPosition";
 import Filters from '@/components/filterBar/Filters';
 import SearchInput from '@/components/filterBar/SearchInput';
+import api from '@/lib/axiosInstance';
 
 export default function CandidatePage() {
   const title = "All candidates";
   const description = "Manage your candidates and detail here.";
   const [filters, setFilters] = useState({});
+  const [allCandidate,setAllCandidate] = useState([]);
 
-  //  useEffect(() =>{
-    //     const fetchPositions = async() =>{
-    //         try {
-    //             const response = await api.get("/api/allPosition",{params:filters});
-    //             setAllPositions(response.data);
-    //         }
-    //         catch (error){
-    //             console.log("Error:",error)
-    //         }
-    //     }
-    //     fetchPositions();
-    // },[])
 
-    const mockData = [
-    { position: "Backend Developer", numberOfPositions: 2, numberOfApplicants: 5, level: "Middle", location: "NZ", deadline: "06/15/2023", status: "Open" },
-    { position: "UI/UX Designer", numberOfPositions: 1, numberOfApplicants: 1, level: "Junior", location: "US", deadline: "06/15/2023", status: "Open" },
-    { position: "Full Stack Developer", numberOfPositions: 2, numberOfApplicants: 3, level: "Senior", location: "VN", deadline: "06/30/2023", status: "Open" },
-    { position: "Data Scientist", numberOfPositions: 4, numberOfApplicants: 4, level: "Lead", location: "VN", deadline: "06/01/2023", status: "Closed" },
-    { position: "DevOps Engineer", numberOfPositions: 2, numberOfApplicants: 8, level: "Intern", location: "VN", deadline: "06/15/2023", status: "Open" },
-  ];
+  useEffect(() =>{
+        const fetchCandidates = async() =>{
+            try {
+                const response = await api.get("candidate/all",{ params: filters });
+                setAllCandidate(response.data.result);
+                console.log(response.data.result)
+            }
+            catch (error){
+                console.log("Error:",error)
+            }
+        }
+        fetchCandidates();
+  },[filters])
 
-    const keySelect = ["status","position","level","location",]
+  const keySelect = [
+    {key:"status", label:"Status"},
+    {key:"positionTitle", label:"Position"},
+    {key:"level",label:"Level"},            
+  ]
 
 
   return (
@@ -49,10 +49,10 @@ export default function CandidatePage() {
         <div className="FilterBar flex justify-between mx-[32px] ">
             <SearchInput onChange={(value) => setSearch(value)} />
             
-            <Filters allPosition={mockData} keyValue={keySelect} filter={setFilters} />
+            <Filters allPosition={allCandidate} keyValue={keySelect} filter={setFilters} />
         </div> 
         <div className="mx-[32px] mt-[24px]">
-            <TableAllPosition Data={mockData} />
+            <TableAllPosition Data={allCandidate} />
         </div>
     </div>
   );
